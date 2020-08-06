2453a32fe6daafdfac028474d61aa918794a6c7e
sago2693 <56797497+sago2693@users.noreply.github.com>
2020-08-06 18:05:55 -0500

    missing upper_panel

---
+++ upper_panel.py
@@ -0,0 +1,9 @@
+import dash_html_components as html
+import dash_daq as daq
+import dash_bootstrap_components as dbc
+
+header = html.Div(html.Img(src='http://talentu.co/wp-content/uploads/2018/12/logo-big-white.png'),style = {"background-color": "#0e0c48",
+"height": "100px"})
+
+
+panel = html.Div([header])
